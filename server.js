dee26814db72f2abea6499007e4d8facfc6412aa
sushmitha <20b01a1248@svecw.edu.in>
2022-09-21 14:30:16 +0530

    crud ope 2

---
+++ server.js
@@ -0,0 +1,92 @@
+const express=require("express");
+const app=express();
+
+const admin=require("firebase-admin")
+const credentials=require("./key.json")
+
+
+admin.initializeApp({
+    credential: admin.credential.cert(credentials)
+})
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+
+const db=admin.firestore()
+
+
+app.post('/create',async (req,res)=>{
+   try{
+       const id=req.body.email;
+       const userJson = {
+           email:req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName
+       };
+       const response = await db.collection("users").add(userJson);
+        res.send(response);
+   }
+   catch(error){
+    res.send(error);
+   }
+})
+
+app.get('/read/all',async (req,res)=>{
+    try{
+        const usersRef = db.collection("users");
+        const response = await usersRef.get();
+        let responseArr = [];
+        response.forEach(doc=>{
+            responseArr.push(doc.data());
+        })
+        res.send(responseArr);
+    }
+    catch(error){
+        res.send(error);
+    }
+})
+
+app.get("/read/:id",async(req,res)=>{
+    try{
+        const userRef = db.collection("users").doc(req.params.id);
+        const response = await usersRef.get();
+        res.send(response.data());
+    }
+    catch(error){
+        res.send(error);
+    }
+})
+
+app.post('/update',async (req,res)=>{
+    try{
+        const id=req.body.id;
+        const  newFirstName="Hello World";
+
+        const userRef = await db.collection("users").doc(id)
+        .update({
+            firstName : newFirstName
+        });
+        res.send(userRef);
+    }
+    catch(error){
+        res.send(error);
+    }
+})
+
+app.delete("/delete/:id",async(req,res)=>{
+    try{
+        const response = await db.collection("users").doc(req.params.id).delete();
+        res.send(response);
+    }
+    catch(error){
+        res.send(error);
+    }
+})
+
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT,(req,res)=>{
+    console.log(`Server is running on the port ${PORT}.`)
+})
